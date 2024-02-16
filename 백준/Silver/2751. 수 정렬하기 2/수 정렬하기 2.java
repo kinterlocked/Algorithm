45a7a4a5713e2b7a2fd72b5e7b4e3622f46ef93a
Yeon Dong Kim <dusehd0525@naver.com>
2024-02-17 02:29:00 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1600 ms, Memory: 114276 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int T = Integer.parseInt(br.readLine());
+
+		int[] arr = new int[T];
+
+		for (int i = 0; i < T; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(arr);
+		for (int i = 0; i < T; i++) {
+			bw.write(arr[i] + "\n");
+		}
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
