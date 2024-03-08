60130ae831ba0f6a644f39a7018a34a9862c3962
Yeon Dong Kim <dusehd0525@naver.com>
2024-03-08 22:18:44 +0900

    [Silver III] Title: 소수 구하기, Time: 212 ms, Memory: 20120 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class Main {
+
+	public static boolean[] prime;
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		
+		prime = new boolean[N + 1];
+		get_prime();
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = M; i <= N; i++) {
+		
+			if(!prime[i]) sb.append(i).append('\n');
+		}
+		System.out.println(sb);
+	}
+ 
+	public static void get_prime() {
+		
+		prime[0] = prime[1] = true;
+		
+		for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+			if(prime[i]) continue;
+			for(int j = i * i; j < prime.length; j += i) {
+				prime[j] = true;
+			}
+		}
+	}
+}
