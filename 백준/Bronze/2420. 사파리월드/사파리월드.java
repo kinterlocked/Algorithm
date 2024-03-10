4b98d9eaffe3e075c644fbc30788cf7fa51e3530
Yeon Dong Kim <dusehd0525@naver.com>
2024-03-11 02:48:33 +0900

    [Bronze V] Title: 사파리월드, Time: 120 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class Main {
+
+	public static boolean[] prime;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        long n = Long.parseLong(st.nextToken());  
+        long m = Long.parseLong(st.nextToken());
+        
+        System.out.print(Math.abs(n-m));  
+    }
+}
