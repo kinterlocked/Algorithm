fa168cbed7b82e4927628e6b272e1bf76c3f6a95
Yeon Dong Kim <dusehd0525@naver.com>
2024-03-21 19:29:59 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 128 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class Main {
+
+	public static void main(String args[]) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		while(true) {
+			String line = br.readLine();
+			if(line == null || line == "") {
+				break;
+			}else {
+				System.out.println(line);
+			}
+		}
+	}
+}
