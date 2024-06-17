559ca1fac5b2aaa23e0501540f11676dd2423f90
Yeon Dong Kim <dusehd0525@naver.com>
2024-06-17 21:32:54 +0900

    [Bronze II] Title: 음계, Time: 128 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String line = br.readLine();
+		String one = "1 2 3 4 5 6 7 8";
+		String two = "8 7 6 5 4 3 2 1";
+		
+		
+		if(line.equals(one)) {
+			System.out.println("ascending");
+		}else if(line.equals(two)) {
+			System.out.println("descending");
+		}else {
+			System.out.println("mixed");
+		}	
+	}
+}
