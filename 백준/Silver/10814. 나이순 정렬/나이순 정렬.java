3cbd3064d77ace0f8ccd992ec61da1aaa4d22c29
Yeon Dong Kim <dusehd0525@naver.com>
2024-07-28 22:30:17 +0900

    [Silver V] Title: 나이순 정렬, Time: 1468 ms, Memory: 52356 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int age;
+        String name;
+
+        int N = Integer.parseInt(br.readLine());
+        Map<Integer, List<String>> map = new TreeMap<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            age = Integer.parseInt(st.nextToken());
+            name = st.nextToken();
+
+            if (!map.containsKey(age)) {
+                map.put(age, new ArrayList<>());
+            }
+            map.get(age).add(name);
+        }
+
+        // Output
+        for (Map.Entry<Integer, List<String>> entry : map.entrySet()) {
+            age = entry.getKey();
+            List<String> names = entry.getValue();
+            for (String result : names) {
+                System.out.println(age + " " + result);
+            }
+        }
+    }
+}
