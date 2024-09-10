896af082a1f268d424a1fe49a1b54f29039744fc
Yeon Dong Kim <dusehd0525@naver.com>
2024-09-10 21:17:38 +0900

    [Bronze II] Title: 다이얼, Time: 100 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String line = br.readLine();
+        int result = 0;
+        for(int i = 0; i < line.length(); i++) {
+            switch(line.charAt(i)) {
+                case 'A','B','C':
+                    result += 3;
+                    break;
+                case 'D','E','F':
+                    result +=4;
+                    break;
+                case 'G','H','I':
+                    result += 5;
+                    break;
+                case 'J','K','L':
+                    result +=6;
+                    break;
+                case 'M','N','O':
+                    result += 7;
+                    break;
+                case 'P','Q','R','S':
+                    result += 8;
+                    break;
+                case 'T','U','V':
+                    result += 9;
+                    break;
+                default:
+                    result += 10;
+                    break;
+            }
+        }
+        System.out.println(result);
+    }
+}
