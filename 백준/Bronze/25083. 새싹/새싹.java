991378a2ad87ed4b0bb0f817b8e31f341601b7e0
Yeon Dong Kim <dusehd0525@naver.com>
2024-09-11 21:07:27 +0900

    [Bronze V] Title: 새싹, Time: 92 ms, Memory: 13860 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String s = "         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |";
+
+        System.out.print(s);
+    }
+}
