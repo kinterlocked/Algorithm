b02d8dd711e571606504f73895c2874b448ab8c6
Yeon Dong Kim <dusehd0525@naver.com>
2025-04-07 22:18:33 +0900

    [Bronze V] Title: 콜센터, Time: 100 ms, Memory: 13940 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,19 @@
+    import java.io.BufferedReader;
+    import java.io.InputStreamReader;
+    import java.math.BigInteger;
+    import java.util.StringTokenizer;
+
+    public class Main {
+        public static void main(String[] args) throws Exception {
+            System.out.print("     /~\\\n" +
+                    "    ( oo|\n" +
+                    "    _\\=/_\n" +
+                    "   /  _  \\\n" +
+                    "  //|/.\\|\\\\\n" +
+                    " ||  \\ /  ||\n" +
+                    "============\n" +
+                    "|          |\n" +
+                    "|          |\n" +
+                    "|          |");
+        }
+    }
