54fb155247a0b30f5e910812c78532884a8cc3e0
Yeon Dong Kim <dusehd0525@naver.com>
2025-04-12 00:23:37 +0900

    [Bronze V] Title: Rook, Time: 104 ms, Memory: 13996 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,27 @@
+    import java.io.BufferedReader;
+    import java.io.InputStreamReader;
+    import java.math.BigInteger;
+    import java.util.StringTokenizer;
+
+    public class Main {
+        public static void main(String[] args) throws Exception {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringBuilder sb = new StringBuilder();
+
+            sb.append("  ___  ___  ___\n" +
+                    "  | |__| |__| |\n" +
+                    "  |           |\n" +
+                    "   \\_________/\n" +
+                    "    \\_______/\n" +
+                    "     |     |\n" +
+                    "     |     |\n" +
+                    "     |     |\n" +
+                    "     |     |\n" +
+                    "     |_____|\n" +
+                    "  __/       \\__\n" +
+                    " /             \\\n" +
+                    "/_______________\\");
+
+            System.out.print(sb.toString());
+        }
+    }
