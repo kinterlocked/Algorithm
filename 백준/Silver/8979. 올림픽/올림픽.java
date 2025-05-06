8c343d9664c199a366840b8acf0facffb8ffaede
Yeon Dong Kim <dusehd0525@naver.com>
2025-05-06 20:17:42 +0900

    [Silver V] Title: 올림픽, Time: 124 ms, Memory: 14664 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -14,46 +13,46 @@ public class Main {
         int K = Integer.parseInt(st.nextToken());
 
         Country[] countries = new Country[N];
-        
-        int num, gold, silver, bronze;
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            num = Integer.parseInt(st.nextToken());
-            gold = Integer.parseInt(st.nextToken());
-            silver = Integer.parseInt(st.nextToken());
-            bronze = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
+            int gold = Integer.parseInt(st.nextToken());
+            int silver = Integer.parseInt(st.nextToken());
+            int bronze = Integer.parseInt(st.nextToken());
             countries[i] = new Country(num, gold, silver, bronze);
         }
 
         Arrays.sort(countries, (a, b) -> {
-            if (b.gold != a.gold){
-                return b.gold - a.gold;
-            }
-            if (b.silver != a.silver){
-                return b.silver - a.silver;
-            }
+            if (b.gold != a.gold) return b.gold - a.gold;
+            if (b.silver != a.silver) return b.silver - a.silver;
             return b.bronze - a.bronze;
         });
 
         int rank = 1;
-        int target = 1;
+        int targetRank = 1;
 
         for (int i = 0; i < N; i++) {
-            if (countries[i].num == K) {
-                target = rank;
-                break;
-            }
-            if (i > 0 && (countries[i].gold != countries[i - 1].gold || 
-                    countries[i].silver != countries[i - 1].silver || 
-                    countries[i].bronze != countries[i - 1].bronze)) {
+            if (i > 0 && (
+                countries[i].gold != countries[i - 1].gold ||
+                countries[i].silver != countries[i - 1].silver ||
+                countries[i].bronze != countries[i - 1].bronze
+            )) {
                 rank = i + 1;
             }
+            if (countries[i].num == K) {
+                targetRank = rank;
+                break;
+            }
         }
-        System.out.println(target);
+        System.out.println(targetRank);
     }
 }
 class Country {
-    int num, gold, silver, bronze;
+    int num;
+    int gold;
+    int silver;
+    int bronze;
 
     Country(int num, int gold, int silver, int bronze) {
         this.num = num;
