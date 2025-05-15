72c2b8167f498e3a2575f46ef6569792d6b78e22
Yeon Dong Kim <dusehd0525@naver.com>
2025-05-16 01:17:05 +0900

    [Bronze V] Title: 제리와 톰, Time: 124 ms, Memory: 16320 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,18 @@
+    import org.w3c.dom.ls.LSOutput;
+
+    import java.io.BufferedReader;
+    import java.io.InputStreamReader;
+    import java.math.BigInteger;
+    import java.util.StringTokenizer;
+
+    public class Main {
+        public static void main(String[] args) throws Exception {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            System.out.println(B - A + " " + B);
+        }
+    }
