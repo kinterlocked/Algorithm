9fa148272b7c1401357412d30da8ee8ae4827440
Yeon Dong Kim <dusehd0525@naver.com>
2025-06-08 23:28:01 +0900

    [Bronze V] Title: 나는 누구인가, Time: 100 ms, Memory: 13964 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.java
@@ -0,0 +1,15 @@
+        import org.w3c.dom.ls.LSOutput;
+
+        import java.io.BufferedReader;
+        import java.io.InputStreamReader;
+        import java.math.BigInteger;
+        import java.util.Arrays;
+        import java.util.Date;
+        import java.util.StringTokenizer;
+
+        public class Main {
+            public static void main(String[] args) throws Exception {
+                System.out.println("I'm Sexy");
+            }
+        }
+
