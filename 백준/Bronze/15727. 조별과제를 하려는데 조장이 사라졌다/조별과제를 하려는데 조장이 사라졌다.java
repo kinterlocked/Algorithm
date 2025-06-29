4062089bb0531f6a4f8c7be5540ce7a2c63f4717
Yeon Dong Kim <dusehd0525@naver.com>
2025-06-30 04:11:30 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 108 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.java
@@ -0,0 +1,25 @@
+        import org.w3c.dom.ls.LSOutput;
+
+        import java.io.BufferedReader;
+        import java.io.InputStreamReader;
+        import java.math.BigInteger;
+        import java.util.Arrays;
+        import java.util.Date;
+        import java.util.StringTokenizer;
+
+        public class Main {
+            public static void main(String[] args) throws Exception {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//                StringTokenizer st = new StringTokenizer(br.readLine());
+                StringBuilder sb = new StringBuilder();
+
+                int N = Integer.parseInt(br.readLine());
+
+                int count = 0;
+                while(N > 0){
+                    N -= 5;
+                    count++;
+                }
+                System.out.println(count);
+            }
+        }
