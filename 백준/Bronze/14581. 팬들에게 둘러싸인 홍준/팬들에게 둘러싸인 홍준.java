0d4c6f26dd42aa45e676bd3eb350b7fb46f0ad7b
Yeon Dong Kim <dusehd0525@naver.com>
2025-07-08 04:04:19 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 112 ms, Memory: 15720 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,26 @@
+        import org.w3c.dom.ls.LSOutput;
+
+        import java.io.BufferedReader;
+        import java.io.InputStreamReader;
+        import java.math.BigInteger;
+        import java.text.DateFormat;
+        import java.text.SimpleDateFormat;
+        import java.util.Arrays;
+        import java.util.Date;
+        import java.util.StringTokenizer;
+        import java.util.TimeZone;
+
+        public class Main {
+            public static void main(String[] args) throws Exception {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//                StringTokenizer st = new StringTokenizer(br.readLine());
+                StringBuilder sb = new StringBuilder();
+
+                String line = br.readLine();
+
+                sb.append(":fan::fan::fan:\n" +
+                        ":fan::" + line +"::fan:\n" +
+                        ":fan::fan::fan:");
+                System.out.println(sb.toString());
+            }
+        }
