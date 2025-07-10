bc5e708261c76955cf409e3f621aac906ad5080d
Yeon Dong Kim <dusehd0525@naver.com>
2025-07-11 04:12:03 +0900

    [Bronze V] Title: 초코바, Time: 108 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.java
@@ -0,0 +1,28 @@
+        import org.w3c.dom.ls.LSOutput;
+
+        import java.io.BufferedReader;
+        import java.io.InputStreamReader;
+        import java.math.BigInteger;
+        import java.text.DateFormat;
+        import java.text.SimpleDateFormat;
+        import java.util.Arrays;
+        import java.util.Date;
+        import java.util.StringTokenizer;
+        import java.util.TimeZone;
+
+        public class Main {
+            public static void main(String[] args) throws Exception {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                StringBuilder sb = new StringBuilder();
+
+                int N = Integer.parseInt(st.nextToken());
+                int M = Integer.parseInt(st.nextToken());
+
+                if(N * 100 >= M){
+                    System.out.println("Yes");
+                }else{
+                    System.out.println("No");
+                }
+            }
+        }
