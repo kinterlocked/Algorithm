51da87f0aa6a25fb65f22ac236493b52b9786c93
Yeon Dong Kim <dusehd0525@naver.com>
2025-08-02 23:21:28 +0900

    [Bronze V] Title: 등록, Time: 100 ms, Memory: 14072 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        
+        System.out.println(421);
+        System.out.println("dusehd0525");
+    }
+}
