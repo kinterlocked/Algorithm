efb172b80f1315b4b3e3ddd0047578a1a69b2018
Yeon Dong Kim <dusehd0525@naver.com>
2025-08-11 02:28:46 +0900

    [Bronze V] Title: 身長 (Height), Time: 112 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/24086. 身長 （Height）/身長 （Height）.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+       int A = Integer.parseInt(br.readLine());
+       int B = Integer.parseInt(br.readLine());
+        System.out.println(B - A);
+    }
+}
