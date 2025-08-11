98993b76b9150cd5c023bf2ac11d68e60a5e5410
Yeon Dong Kim <dusehd0525@naver.com>
2025-08-11 20:28:41 +0900

    [Bronze V] Title: 라면 공식, Time: 120 ms, Memory: 15908 KB -BaekjoonHub

---
+++ 백준/Bronze/30007. 라면 공식/라면 공식.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int X = Integer.parseInt(st.nextToken()) - 1;
+
+            int result = (A * X) + B;
+            sb.append(result + "\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
