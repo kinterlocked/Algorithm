ba855af29c13bd04894ffa65f8c74ac815b0c18b
Yeon Dong Kim <dusehd0525@naver.com>
2025-08-14 02:27:40 +0900

    [Bronze IV] Title: I Speak TXTMSG, Time: 96 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/6841. I Speak TXTMSG/I Speak TXTMSG.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("CU", "see you");
+        map.put(":-)", "I’m happy");
+        map.put(":-(", "I’m unhappy");
+        map.put(";-)", "wink");
+        map.put(":-P", "stick out my tongue");
+        map.put("(~.~)", "sleepy");
+        map.put("TA", "totally awesome");
+        map.put("CCC", "Canadian Computing Competition");
+        map.put("CUZ", "because");
+        map.put("TY", "thank-you");
+        map.put("YW", "you’re welcome");
+        map.put("TTYL", "talk to you later");
+
+        while (true) {
+            String command = br.readLine();
+            if (map.containsKey(command)) {
+                sb.append(map.get(command) + "\n");
+            } else {
+                sb.append(command + "\n");
+            }
+            if (command.equals("TTYL")){
+                break;
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
