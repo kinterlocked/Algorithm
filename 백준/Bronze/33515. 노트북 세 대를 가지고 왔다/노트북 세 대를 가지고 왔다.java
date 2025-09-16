2a88877f8de2b60853563ff6cd48fb7056869a43
Yeon Dong Kim <dusehd0525@naver.com>
2025-09-17 03:44:25 +0900

    [Bronze V] Title: 노트북 세 대를 가지고 왔다, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/33515. 노트북 세 대를 가지고 왔다/노트북 세 대를 가지고 왔다.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.SQLOutput;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int T1 = Integer.parseInt(st.nextToken());
+        int T2 = Integer.parseInt(st.nextToken());
+
+        System.out.println(Math.min(T1, T2));
+    }
+}
