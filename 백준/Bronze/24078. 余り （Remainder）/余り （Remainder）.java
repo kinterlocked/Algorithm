d7ced6d545878e50ab6cd3ae2d5bf22ac3fad697
Yeon Dong Kim <dusehd0525@naver.com>
2025-09-30 00:14:12 +0900

    [Bronze V] Title: 余り (Remainder), Time: 108 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Bronze/24078. 余り （Remainder）/余り （Remainder）.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.SQLOutput;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st ;
+        StringBuilder sb = new StringBuilder();
+
+
+        int X = Integer.parseInt(br.readLine());
+        System.out.println(X % 21);
+    }
+}
