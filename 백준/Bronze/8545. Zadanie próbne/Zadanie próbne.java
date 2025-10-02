9733ae35298a47e09c7088b825a6605dfd4b10fa
Yeon Dong Kim <dusehd0525@naver.com>
2025-10-03 02:32:03 +0900

    [Bronze V] Title: Zadanie próbne, Time: 100 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/8545. Zadanie próbne/Zadanie próbne.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.SQLOutput;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st ;
+        StringBuilder sb = new StringBuilder();
+
+        String word = br.readLine();
+        sb.append(word).reverse();
+        System.out.println(sb.toString());
+    }
+}
