d0095ba4b74cf4d99f310aba075fc11e5bbffcd2
Yeon Dong Kim <dusehd0525@naver.com>
2025-10-06 01:14:10 +0900

    [Bronze V] Title: Roller Coaster Ride, Time: 100 ms, Memory: 14076 KB -BaekjoonHub

---
+++ 백준/Bronze/34454. Roller Coaster Ride/Roller Coaster Ride.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.SQLOutput;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st ;
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+        int P = Integer.parseInt(br.readLine());
+        
+        if(C * P >= N){
+            System.out.println("yes");
+        }else{
+            System.out.println("no");
+        }
+    }
+}
