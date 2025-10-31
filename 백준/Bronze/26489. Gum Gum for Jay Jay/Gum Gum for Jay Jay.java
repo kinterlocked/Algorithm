c267146584a3a6f72f302cc616a4b3900afb0f5e
Yeon Dong Kim <dusehd0525@naver.com>
2025-10-31 17:43:38 +0900

    [Bronze V] Title: Gum Gum for Jay Jay, Time: 100 ms, Memory: 13968 KB -BaekjoonHub

---
+++ 백준/Bronze/26489. Gum Gum for Jay Jay/Gum Gum for Jay Jay.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.SQLOutput;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        int count = 0;
+
+        while ((line = br.readLine()) != null) {
+            if (line.startsWith("gum")) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
