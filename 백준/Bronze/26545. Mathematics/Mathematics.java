5ec2ceb009daafb03ac85ec82e9e82612aead6bc
Yeon Dong Kim <dusehd0525@naver.com>
2025-11-02 03:00:43 +0900

    [Bronze V] Title: Mathematics, Time: 104 ms, Memory: 13996 KB -BaekjoonHub

---
+++ 백준/Bronze/26545. Mathematics/Mathematics.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.SQLOutput;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int total = 0;
+        for(int i = 0; i < n; i++) {
+            int a = Integer.parseInt(br.readLine());
+            total += a;
+        }
+        System.out.println(total);
+    }
+}
