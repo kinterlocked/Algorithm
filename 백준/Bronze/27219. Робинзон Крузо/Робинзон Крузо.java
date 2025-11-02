477d4581fcfe5d5302afebd53621158f67e05dc5
Yeon Dong Kim <dusehd0525@naver.com>
2025-11-03 00:43:59 +0900

    [Bronze V] Title: Робинзон Крузо, Time: 148 ms, Memory: 14724 KB -BaekjoonHub

---
+++ 백준/Bronze/27219. Робинзон Крузо/Робинзон Крузо.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.SQLOutput;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 0; i < n /5 ; i++) {
+            System.out.print("V");
+        }
+        for(int i = 0; i < n % 5; i++) {
+            System.out.print("I");
+        }
+    }
+}
