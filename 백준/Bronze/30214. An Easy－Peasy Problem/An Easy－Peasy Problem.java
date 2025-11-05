5c8116cafd8ac6f90d396add2418d13677b6c2e0
Yeon Dong Kim <dusehd0525@naver.com>
2025-11-06 02:57:19 +0900

    [Bronze V] Title: An Easy-Peasy Problem, Time: 100 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/30214. An Easy－Peasy Problem/An Easy－Peasy Problem.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.SQLOutput;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        double s1 = Double.parseDouble(st.nextToken());
+        double s2 = Double.parseDouble(st.nextToken());
+
+        if(s1 >= s2 / 2) {
+            System.out.println("E");
+        }else {
+            System.out.println("H");
+        }
+    }
+}
