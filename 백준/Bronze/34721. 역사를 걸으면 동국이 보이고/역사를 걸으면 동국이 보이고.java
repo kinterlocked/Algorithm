b8e49c1025069498525ef3b905f0b16b6f69e67c
Yeon Dong Kim <dusehd0525@naver.com>
2025-11-12 03:48:04 +0900

    [Bronze V] Title: 역사를 걸으면 동국이 보이고, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/34721. 역사를 걸으면 동국이 보이고/역사를 걸으면 동국이 보이고.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.SQLOutput;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        while (N-- > 0) {
+            sb.append("I love DGU\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
