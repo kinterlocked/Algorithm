f657a6c0082aec2c68966c8f2539befac0edec7c
Yeon Dong Kim <dusehd0525@naver.com>
2025-11-14 02:37:52 +0900

    [Bronze V] Title: Call for Problems, Time: 100 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Bronze/32498. Call for Problems/Call for Problems.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.SQLOutput;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        for(int i = 0; i < n; i++) {
+            int a = Integer.parseInt(br.readLine());
+            if(a % 2 == 1) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
