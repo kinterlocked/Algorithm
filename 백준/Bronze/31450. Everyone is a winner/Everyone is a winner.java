45bb89db6588fc85ded95993a75c411c378e070d
Yeon Dong Kim <dusehd0525@naver.com>
2025-11-19 05:15:33 +0900

    [Bronze V] Title: Everyone is a winner, Time: 100 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/31450. Everyone is a winner/Everyone is a winner.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.SQLOutput;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        if (m % k == 0) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
