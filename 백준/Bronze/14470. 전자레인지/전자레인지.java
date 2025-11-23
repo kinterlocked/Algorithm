59a449d6d6c111f9e5e6b16d680160336dc93660
Yeon Dong Kim <dusehd0525@naver.com>
2025-11-24 02:50:55 +0900

    [Bronze IV] Title: 전자레인지, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/14470. 전자레인지/전자레인지.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.SQLOutput;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+        int D = Integer.parseInt(br.readLine());
+        int E = Integer.parseInt(br.readLine());
+
+        int sec = 0;
+
+        if (A < 0) {
+            sec = (A * C * -1) + (B * E) + D;
+        } else {
+            sec = (B - A) * E;
+        }
+        System.out.println(sec);
+    }
+}
