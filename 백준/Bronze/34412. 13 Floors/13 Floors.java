edea45749ff453b874bb16e57da919890474c55a
Yeon Dong Kim <dusehd0525@naver.com>
2025-11-29 03:53:43 +0900

    [Bronze V] Title: 13 Floors, Time: 100 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/34412. 13 Floors/13 Floors.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.SQLOutput;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        
+        int x = Integer.parseInt(br.readLine());
+        if (x < 13) {
+            System.out.println(x);
+        } else {
+            System.out.println(x + 1);
+        }
+    }
+}
