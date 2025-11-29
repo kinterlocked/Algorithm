123cf5e5217355aae95d2dd81cb30693e3b1d9f4
Yeon Dong Kim <dusehd0525@naver.com>
2025-11-30 04:04:55 +0900

    [Bronze V] Title: M-Climb, Time: 96 ms, Memory: 14040 KB -BaekjoonHub

---
+++ 백준/Bronze/34411. M－Climb/M－Climb.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.SQLOutput;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int m = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+
+        System.out.print(m * n);
+    }
+}
