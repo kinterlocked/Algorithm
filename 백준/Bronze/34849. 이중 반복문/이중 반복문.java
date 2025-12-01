5fcaa3d99776a2491e4634fa4e64bc95f6ba4d65
Yeon Dong Kim <dusehd0525@naver.com>
2025-12-02 02:43:29 +0900

    [Bronze V] Title: 이중 반복문, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/34849. 이중 반복문/이중 반복문.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.SQLOutput;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(N <= 10000 ? "Accepted" : "Time limit exceeded");
+    }
+}
