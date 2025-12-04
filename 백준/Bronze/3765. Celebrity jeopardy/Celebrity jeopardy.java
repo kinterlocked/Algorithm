d82e520b43c55564e9a60fcee5cf6e8f332bc928
Yeon Dong Kim <dusehd0525@naver.com>
2025-12-05 04:25:18 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 100 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.SQLOutput;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        String command;
+        while ((command = br.readLine()) != null) {
+            sb.append(command + "\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
