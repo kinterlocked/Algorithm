a6a8549d8b09110c13bdbd7a9e076425a59d2ce2
Yeon Dong Kim <dusehd0525@naver.com>
2025-12-15 03:39:03 +0900

    [Bronze IV] Title: Dedupe, Time: 184 ms, Memory: 17472 KB -BaekjoonHub

---
+++ 백준/Bronze/5357. Dedupe/Dedupe.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.SQLOutput;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        Scanner sc =new Scanner(System.in);
+        int n = sc.nextInt();
+        int j;
+        for(int i = 0; i < n; i++) {
+            String a = sc.next();
+            for(j = 0; j < a.length() - 1; j++) {
+                if(a.charAt(j) != a.charAt(j + 1)) {
+                    System.out.print(a.charAt(j));
+                }
+            }
+            System.out.println(a.charAt(j));
+        }
+    }
+}
