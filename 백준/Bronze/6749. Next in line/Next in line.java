99a116932f0c9bbdd4d6dedaeaa846e2f6f3b4d2
Yeon Dong Kim <dusehd0525@naver.com>
2025-12-18 04:02:50 +0900

    [Bronze IV] Title: Next in line, Time: 172 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.SQLOutput;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        Scanner sc =new Scanner(System.in);
+
+        int youngest = sc.nextInt();
+        int middle = sc.nextInt();
+        sc.close();
+
+        System.out.println(middle + (middle - youngest));
+    }
+}
