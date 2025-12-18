defdaa642088b454f8bafe4ae23c1935d4fe35f1
Yeon Dong Kim <dusehd0525@naver.com>
2025-12-19 04:18:50 +0900

    [Bronze IV] Title: Speed fines are not fine!, Time: 168 ms, Memory: 17576 KB -BaekjoonHub

---
+++ 백준/Bronze/6763. Speed fines are not fine！/Speed fines are not fine！.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.SQLOutput;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        Scanner sc =new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a >= b) {
+            System.out.println("Congratulations, you are within the speed limit!");
+        }else {
+            if(1 <= b - a && b - a <= 20) {
+                System.out.println("You are speeding and your fine is $100.");
+            }else if(21 <= b - a && b - a <= 30) {
+                System.out.println("You are speeding and your fine is $270.");
+            }else if(31 <= b - a) {
+                System.out.println("You are speeding and your fine is $500.");
+            }
+        }
+    }
+}
