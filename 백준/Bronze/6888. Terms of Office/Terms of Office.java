9518ff8ca5136fc04a7f580ccd6d7c1fd7afb3a1
Yeon Dong Kim <dusehd0525@naver.com>
2025-12-26 05:22:02 +0900

    [Bronze IV] Title: Terms of Office, Time: 184 ms, Memory: 18224 KB -BaekjoonHub

---
+++ 백준/Bronze/6888. Terms of Office/Terms of Office.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.SQLOutput;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        Scanner sc =new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        for(int i = a; i <= b; i = i + 60) {
+            System.out.println("All positions change in year " + i);
+        }
+    }
+}
