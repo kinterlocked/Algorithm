719def532324525b40d4b30a25e249affd2e4ece
Yeon Dong Kim <dusehd0525@naver.com>
2026-01-04 03:26:45 +0900

    [Bronze IV] Title: Quadrilateral, Time: 168 ms, Memory: 17800 KB -BaekjoonHub

---
+++ 백준/Bronze/10188. Quadrilateral/Quadrilateral.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.SQLOutput;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        Scanner sc =new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            for(int j = 0; j < b; j++) {
+                for(int k = 0; k < a; k++) {
+                    System.out.print("X");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+}
