e6405c3adbfbb15c1103633f824601ce554d2bd2
Yeon Dong Kim <dusehd0525@naver.com>
2026-01-09 04:15:17 +0900

    [Bronze IV] Title: 뜨거운 붕어빵, Time: 168 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.SQLOutput;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        Scanner sc =new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        if(n > 0 && m > 0) {
+            for(int i = 0; i < n; i++) {
+                String a = sc.next();
+                for(int j = m - 1; j >= 0; j--) {
+                    System.out.print(a.charAt(j));
+                }
+                System.out.println();
+            }
+        }
+    }
+}
