6fc3fd5d360d62e95a29ced5f7a9563c7605a3e9
Yeon Dong Kim <dusehd0525@naver.com>
2026-01-10 04:14:46 +0900

    [Bronze IV] Title: 한글 2, Time: 164 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/11283. 한글 2/한글 2.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.sql.SQLOutput;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        Scanner sc =new Scanner(System.in);
+        String a = sc.next();
+        System.out.println((int)(a.charAt(0) - 44031));
+    }
+}
