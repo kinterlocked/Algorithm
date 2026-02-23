33c622adf40a5367ff26f7b5015b5249435d79be
Yeon Dong Kim <dusehd0525@naver.com>
2026-02-24 03:44:38 +0900

    [Bronze IV] Title: Animal King Election, Time: 168 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/17598. Animal King Election/Animal King Election.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int l = 0;
+		int t = 0;
+		for(int i = 0; i < 9; i++) {
+			String a = sc.next();
+			if(a.equals("Lion")) {
+				l++;
+			}else {
+				t++;
+			}
+		}
+		
+		if(l > t) {
+			System.out.println("Lion");
+		}else {
+			System.out.println("Tiger");
+		}
+		sc.close();
+	}
+}
