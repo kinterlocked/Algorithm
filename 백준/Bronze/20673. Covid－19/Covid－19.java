a4ae8175e864f1166e1b485aca0ed26138a02ef3
Yeon Dong Kim <dusehd0525@naver.com>
2026-03-15 04:38:39 +0900

    [Bronze IV] Title: Covid-19, Time: 172 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/20673. Covid－19/Covid－19.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int p = sc.nextInt();
+		int q = sc.nextInt();
+		
+		if(p <= 50 && q <= 10) {
+			System.out.println("White");
+		}else if(q > 30) {
+			System.out.println("Red");
+		}else {
+			System.out.println("Yellow");
+		}
+		sc.close();
+	}
+}
