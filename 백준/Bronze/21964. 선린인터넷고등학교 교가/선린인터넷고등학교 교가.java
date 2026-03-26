f39d0b1b66f87e4327af67a2eea1e5c1a393b311
Yeon Dong Kim <dusehd0525@naver.com>
2026-03-27 04:27:50 +0900

    [Bronze IV] Title: 선린인터넷고등학교 교가, Time: 240 ms, Memory: 20828 KB -BaekjoonHub

---
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		String a = sc.next();
+		
+		for(int i = a.length() - 5; i < a.length(); i++) {
+			System.out.print(a.charAt(i));
+		}
+		sc.close();
+	}
+}
