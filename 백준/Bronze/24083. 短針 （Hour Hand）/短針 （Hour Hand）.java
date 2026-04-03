e16498b8f5274878a7746ef156d649a8fb4ca0e2
Yeon Dong Kim <dusehd0525@naver.com>
2026-04-04 03:50:59 +0900

    [Bronze IV] Title: 短針 (Hour Hand), Time: 184 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/24083. 短針 （Hour Hand）/短針 （Hour Hand）.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		if((a + b) % 12 != 0) {
+			System.out.println((a + b) % 12);
+		}else {
+			System.out.println("12");
+		}
+		sc.close();
+	}
+}
